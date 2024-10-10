e6155c34fc0d0437c6d81bd7fdb443e851fbccd0
Potc1 <103571711+Potc1@users.noreply.github.com>
2024-10-10 16:14:53 +0300

    Revert "1"

--- src/lection_1/Main.java
+++ src/lection_1/Main.java
@@ -19,13 +19,12 @@ public class Main {
     public static void main(String[] args) {
         Supercar Camaro = new Supercar("camaro",  200, "chevy", 2000, 0);
         //System.out.println(Camaro.GetPrice());
-        //Camaro.StageTunning();123
+        //Camaro.StageTunning();
         Camaro.SetStage(2);
         Supercar.showDiscont();
         System.out.println(Camaro.GetPrice());
         Camaro.display();
         Camaro.ShowAboutSupercar();
         SUV.AvaliableForTunningOptions();
-        ///SUV.AvaliableForTunningOptions();
     }
 }
